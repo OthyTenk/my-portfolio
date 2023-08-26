16d6a4735026a7e5e2d788f4a82993750f088b3f
Othy Tenk <othy.tenk@gmail.com>
2023-08-26 23:35:43 +0800

    feat: added the mobile menu

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,15 +1,14 @@
-'use client';
+"use client";
 
-import { Disclosure } from '@headlessui/react'
-import Link from 'next/link'
-import React from 'react'
-import { usePathname } from 'next/navigation'
-import { ThemeSwitcher } from './ThemeSwitcher';
-
+import { Disclosure } from "@headlessui/react";
+import Link from "next/link";
+import React from "react";
+import { usePathname } from "next/navigation";
+import { ThemeSwitcher } from "./ThemeSwitcher";
 
 export const Navbar = () => {
-    const pathname = usePathname() || '/';
-    return (
+  const pathname = usePathname() || "/";
+  return (
     <Disclosure as="nav">
       {({ open }) => (
         <>
@@ -52,11 +51,74 @@ export const Navbar = () => {
                   <ThemeSwitcher />
                 </div>
               </div>
+
+              <div className="-mr-2 flex items-center sm:hidden">
+                <ThemeSwitcher />
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-orange-500 dark:hover:bg-gray-800">
+                  {open ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                      />
+                    </svg>
+                  )}
+                </Disclosure.Button>
+              </div>
             </div>
           </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="pt-2 pb-3 space-y-1">
+              <Link
+                href="/"
+                prefetch
+                className={`${
+                  pathname == "/"
+                    ? "bg-orange-50  border-orange-500 text-orange-500 block pl-3 pr-4 py-2 border-l-4 text-base font-medium dark:bg-gray-800"
+                    : "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-orange-500 block pl-3 pr-4 py-2 dark:hover:bg-gray-700 border-l-4 text-base font-medium dark:text-white"
+                } `}
+              >
+                Home
+              </Link>
+              <Link
+                href="/projects"
+                prefetch
+                className={`${
+                  pathname == "/projects"
+                    ? "bg-orange-50 border-orange-500 text-orange-500 block pl-3 pr-4 py-2 border-l-4 text-base font-medium dark:bg-gray-800"
+                    : "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-orange-500 block pl-3 pr-4 py-2 dark:hover:bg-gray-700 border-l-4 text-base font-medium dark:text-white"
+                } `}
+              >
+                Projects
+              </Link>
+            </div>
+          </Disclosure.Panel>
         </>
       )}
     </Disclosure>
   );
-}
-
+};
