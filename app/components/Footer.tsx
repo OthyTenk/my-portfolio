cebcdfc5a1ac0594052d22ad43120ff083d7ac72
Othy Tenk <othy.tenk@gmail.com>
2023-08-27 14:51:41 +0800

    fix: changed the text color of the footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,18 +3,18 @@ import React from 'react'
 export const Footer = () => {
   return (
     <footer className="mt-20">
-      <div className="max-w-6xl mx-auto flex lg:flex-row flex-col items-center lg:justify-between justify-center gap-y-4 md:px-16 px-6 py-16 text-zinc-400">
-        <small className="duration-200 font-mono">
+      <div className="max-w-6xl mx-auto flex lg:flex-row flex-col items-center lg:justify-between justify-center gap-y-4 md:px-16 px-6 py-16 text-zinc-600">
+        <small className="duration-200">
           All rights reserved <span className='text-orange-500'>&copy;</span> {new Date().getFullYear()}
         </small>
 
-        <small className="hover:text-white duration-200">
+        <small className="hover:text-zinc-400 dark:hover:text-white duration-200">
           <a
             href="https://github.com/OthyTenk/my-portfolio"
             target="_blank"
             rel="noreferrer noopener"
           >
-            Prepared by <span className="text-orange-400">OkDo</span>
+            Prepared by <span className="text-orange-500">OkDo</span>
           </a>
         </small>
       </div>
