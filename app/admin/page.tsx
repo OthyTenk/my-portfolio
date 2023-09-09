a0e9c6058609d980ec20f2a9392d68b73474059f
OkDo <34827155+OthyTenk@users.noreply.github.com>
2023-09-09 22:18:54 +0800

    feat/adds the admin page and forward to studio (#5)

---
+++ app/admin/page.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import { PageHeader } from "../components";
+
+const page = async () => {
+  
+
+  return (
+    <div className="divide-y divide-gray-200 dark:divide-gray-700">
+      <PageHeader title="All Projects" />
+
+      <div className="grid gap-y-8 sm:gap-6  sm:grid-cols-2 md:gap-6 lg:grid-cols-3 lg:gap-10 pt-8">
+        <Link href="/">login to dashboard</Link>
+      </div>
+    </div>
+  );
+};
+
+export default page;
