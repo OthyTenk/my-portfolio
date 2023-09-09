5ec176d4f97432d0bace0a901262dd55a85dda4c
Othy Tenk <othy.tenk@gmail.com>
2023-09-10 00:07:55 +0800

    fix: update sanity config file

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig } from 'sanity'
-import { deskTool } from 'sanity/desk'
-import { schemaTypes } from './app/schemas'
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+import { schemaTypes } from "./app/schemas";
 
 export default defineConfig({
-  name: 'sanity-project',
-  title: 'Sanity project',
+  name: "sanity-project",
+  title: "OkDo Portfolio",
 
-  projectId: 'c5g75h7y',
-  dataset: 'production',
+  projectId: "c5g75h7y",
+  dataset: "production",
 
   plugins: [deskTool()],
 
-  basePath: '/studio',
+  basePath: "/studio",
 
   schema: {
     types: schemaTypes,
   },
-})
+});
